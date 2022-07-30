55001111540cf357c9631c60e945aa41f0846a14
Mozes Elbarbary <moz@moz.local>
2022-07-30 13:28:04 +1200

    Creating api handlers

---
+++ server/controllers/activitiesController.js
@@ -0,0 +1,32 @@
+const Activities = require('../models/activitiesModel');
+
+/**
+ * It's an async function that uses the Activity model to find all activities and then returns a status of 200 with the activities in the response body.
+ */
+const getActivities = async (req, res) => {
+  try {
+    const allActivities = await Activities.find();
+    res.status(200).json(allActivities);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+/**
+ * It creates a new activity and saves it to the database.
+ */
+const addActivity = async (req, res) => {
+  const activity = new Activities(req.body);
+
+  try {
+    const newActivity = await activity.save();
+    res.status(201).json(newActivity);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+module.exports = {
+  getActivities,
+  addActivity,
+};
