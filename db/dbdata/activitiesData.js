f64a452d3ef88750244facdc3f556db92bcd90b9
Mozes Elbarbary <moz@moz.local>
2022-07-31 16:45:50 +1200

    Persistent data

---
+++ db/dbdata/activitiesData.js
@@ -0,0 +1,12 @@
+const activitiesData = [
+    {
+        name: 'test-activity',
+        time: '1 hr'
+    },
+    {   
+        name: 'test-activity',
+        time: '1 hr'
+    },
+
+]
+module.exports = activitiesData
